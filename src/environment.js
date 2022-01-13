9f3d6d11271e0522720e0b3b7c0b1ccd8f602271
Pablo A. Castillo Ramirez <pablo@dessausoftware.com>
2022-01-13 08:51:46 -0600

    mend

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ export const environment = {
     api: {
         identity: {
             // url: 'http://localhost:3030/api', // Local
-            url: 'https://vx-api-identity.herokuapp.com/api', // Development
+            url: 'https://vx-api-identity.herokuapp.com/api', // Production
         },
     },
 }
